88e27bcc0fbfdeb5bf3f90f48d9d194b67ef45ab
Samantha Lin <linsam@engr.orst.edu>
2025-01-17 06:59:09 -0800

    Add API endpoints for caretaker-pets and caretaker-contacts nested relationships.

--- django_project/caretakers/api/urls.py
+++ django_project/caretakers/api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 
-from .views import CaretakerViewSet
+from .views import CaretakerViewSet, CaretakerPetsViewSet, CaretakerContactsViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'caretakers', CaretakerViewSet)
 
+caretakers_router = routers.NestedDefaultRouter(router, r'caretakers', lookup='caretaker')
+caretakers_router.register(r'pets', CaretakerPetsViewSet, basename='caretaker-pets')
+caretakers_router.register(r'contacts', CaretakerContactsViewSet, basename='caretaker-contacts')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(caretakers_router.urls)),
 ]
--- django_project/caretakers/api/views.py
+++ django_project/caretakers/api/views.py
@@ -3,6 +3,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from ..models import Caretaker
 from .serializers import CaretakerSerializer
+from pets.models import Pet
+from pets.api.serializers import PetSerializer
+from contacts.models import Contact
+from contacts.api.serializers import ContactSerializer
 
 
 class CaretakerViewSet(viewsets.ModelViewSet):
@@ -10,3 +14,15 @@ class CaretakerViewSet(viewsets.ModelViewSet):
     serializer_class = CaretakerSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id']
+
+class CaretakerPetsViewSet(viewsets.ModelViewSet):
+    serializer_class = PetSerializer
+
+    def get_queryset(self):
+        return Pet.objects.filter(caretakers__in=self.kwargs['caretaker_pk'])
+
+class CaretakerContactsViewSet(viewsets.ModelViewSet):
+    serializer_class = ContactSerializer
+
+    def get_queryset(self):
+        return Contact.objects.filter(caretakers__in=self.kwargs['caretaker_pk'])
