c9c1f576208e46ebc571a239258703f57e584d2e
Samantha Lin <linsam@engr.orst.edu>
2025-03-21 07:25:08 -0700

    Create EntryRow component to pass into EntryList. Display entry details with placeholder info and icon.

--- pawprint_client/components/EntryList.tsx
+++ pawprint_client/components/EntryList.tsx
@@ -3,18 +3,15 @@ import { FlatList, View, Text, StyleSheet } from 'react-native';
 import { THEME } from '@/theme';
 import { Entry } from '@/api_interfaces';
 import { useStoreContext } from '@/components/StoreContext';
+import EntryRow from '@/components/EntryRow';
+
 
 export default function EntryList() {
     const { state, dispatch } = useStoreContext();
 
     const renderItem = ({item} : {item:Entry}) => {
         return (
-            <View style={styles.entryRow}>
-                <Text style={styles.titleText}>{item.title}</Text>
-                <Text style={styles.subtitleText}>for pets {item.pets}</Text>
-                <Text style={styles.subtitleText}>with caretakers {item.caretakers}</Text>
-                <Text style={styles.subtitleText}>on {item.recorded_on}</Text>
-            </View>
+            <EntryRow entry={item}/>
         )
     }
 
@@ -39,16 +36,5 @@ const styles = StyleSheet.create({
     },
     spacer: {
         height: 38,
-    },
-    entryRow: {
-        margin: 12,
-    },
-    titleText: {
-        color: THEME.COLOR_DARK_BLUE,
-        fontSize: 14,
-    },
-    subtitleText: {
-        color: THEME.COLOR_DARK_BLUE,
-        fontSize: 12,
     },
 })
---
+++ pawprint_client/components/EntryRow.tsx
@@ -0,0 +1,99 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+import { THEME } from '@/theme';
+import { Entry } from '@/api_interfaces';
+
+
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+function convertDateTime(recorded_on:string) {
+    const datetime = new Date(recorded_on)
+    const month = MONTHS[datetime.getMonth()]
+    const day = datetime.getDate().toString().padStart(2, '0')
+    const year = datetime.getFullYear()
+    const date = `${month} ${day}, ${year}`
+    const hours = datetime.getHours() % 12 == 0 ? 12 : datetime.getHours() % 12
+    const minutes = datetime.getMinutes().toString().padStart(2, '0')
+    const am_pm = datetime.getHours() < 12 ? 'am' : 'pm'
+    const time = `${hours}:${minutes} ${am_pm}`
+    return [date, time]
+}
+
+export default function EntryRow({entry} : {entry:Entry}) {
+    const [date, time] = convertDateTime(entry.recorded_on)
+
+    return (
+        <Pressable
+            onPress={() => alert(`entry - ${entry.title}`)}
+            style={styles.entryRow}>
+
+            <View style={styles.kindIcon}>
+                <FontAwesome5
+                    name='utensils'
+                    color={THEME.COLOR_OTHER}
+                    size={24}
+                />
+            </View>
+            <View style={styles.textInfo}>
+                <View style={styles.headerRow}>
+                    <Text style={[styles.titleText, styles.leftText]}>{entry.title}</Text>
+                    <Text style={styles.secondaryText}>{time}</Text>
+                </View>
+                <Text style={styles.secondaryText}>placeholder notes / measurement text</Text>
+                <Text style={styles.primaryText}>with caretakers{entry.caretakers}</Text>
+            </View>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    entryRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 12,
+        paddingBottom: 12,
+        paddingLeft: 16,
+        paddingRight: 16,
+        backgroundColor: THEME.COLOR_WHITE,
+        borderBottomWidth: 1,
+        borderColor: THEME.COLOR_LIGHT_BLUE,
+    },
+    kindIcon: {
+        height: 45,
+        width: 45,
+        borderRadius: 45 / 2,
+        borderWidth: 1,
+        borderColor: THEME.COLOR_DARK_BLUE,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 16,
+    },
+    textInfo: {
+        flex: 1,
+        gap: 2,
+    },
+    titleText: {
+        color: THEME.COLOR_DARK_BLUE,
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+    primaryText: {
+        color: THEME.COLOR_DARK_BLUE,
+        fontSize: 12,
+    },
+    secondaryText: {
+        color: THEME.COLOR_MEDIUM_GREY,
+        fontSize: 12,
+    },
+    headerRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    leftText: {
+        textAlign: 'left',
+    },
+    rightText: {
+        textAlign: 'right',
+    }
+})
