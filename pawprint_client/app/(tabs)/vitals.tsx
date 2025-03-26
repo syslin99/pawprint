2e9a4fbcebeeaa988a2daa16893ea1e3f9d28c22
Samantha Lin <linsam@engr.orst.edu>
2025-03-26 08:04:49 -0700

    Revert Vitals page to skeleton page

--- pawprint_client/app/(tabs)/vitals.tsx
+++ pawprint_client/app/(tabs)/vitals.tsx
@@ -1,23 +1,19 @@
 import { View, ScrollView, Text, StyleSheet} from 'react-native';
 
 import { THEME } from '@/theme';
-import { useStoreContext } from '@/components/StoreContext';
 import MainHeader from '@/components/MainHeader';
 
 
 export default function Vitals() {
-    const { state, dispatch } = useStoreContext();
-    console.log(Array.from(state.vitals.keys()));
-
     return (
-        <View style={styles.screen}>
-            <MainHeader title='Vitals' />
+        <View style={styles.container}>
+            <MainHeader title='Vitals'/>
             <ScrollView style={styles.container}>
                 <Text style={styles.text}>Vitals Screen</Text>
             </ScrollView>
         </View>
     );
-}
+    }
 
 const styles = StyleSheet.create({
     screen: {
