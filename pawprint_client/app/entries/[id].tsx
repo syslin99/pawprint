09274e2d684470444120b301875db260aada255f
Samantha Lin <linsam@engr.orst.edu>
2025-03-28 08:04:20 -0700

    Add photo carousel to Entry Detail page

--- pawprint_client/app/entries/[id].tsx
+++ pawprint_client/app/entries/[id].tsx
@@ -1,5 +1,9 @@
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Animated, { useSharedValue } from 'react-native-reanimated';
+import { useRef } from 'react';
+import Carousel, { ICarouselInstance, Pagination } from 'react-native-reanimated-carousel';
 
 import { THEME } from '@/theme';
 import { convertDateTime, formatMeasurement } from '@/functions';
@@ -21,39 +25,100 @@ export default function EntryDetail() {
     const [date, time] = convertDateTime(entry.recorded_on)
     const measurement = formatMeasurement(entry.kind.name, entry.measurement)
 
+    const progress = useSharedValue<number>(0);
+    const ref = useRef<ICarouselInstance>(null);
+    const renderItem = ({item} : {item:string}) => (
+        <Animated.Image
+            source={{uri: item}}
+            style={{height: 300, width: windowWidth, borderRadius: 20}}
+        />
+    )
+    const onPressPagination = (index:number) => {
+        ref.current?.scrollTo({
+            count: index - progress.value,
+            animated: false,
+        })
+    }
+    const windowWidth = Dimensions.get('window').width;
+
     return (
-        <View style={styles.screen}>
+        <GestureHandlerRootView style={styles.screen}>
             <SecondaryHeader title='Entry Detail' hasEditActions={true} />
             <ScrollView
-                style={styles.container}
                 contentContainerStyle={{ gap: 8 }}
+                stickyHeaderIndices={[0]}
             >
-                {/* General Information */}
-                <View style={styles.generalInfo}>
-                    <Text style={styles.titleText}>{entry.title}</Text>
-                    <Text style={styles.bodyText}>for {pet_names}</Text>
-                    <Text style={styles.bodyText}>with {caretaker_names}</Text>
-                    <Text style={styles.bodyText}>on {date} at {time}</Text>
+                {/* Photo Carousel */}
+                {entry.pictures.length > 0 &&
+                <View style={styles.carouselContainer}>
+                    <Carousel
+                        data={entry.pictures}
+                        height={300}
+                        width={windowWidth}
+                        style={{width: windowWidth}}
+                        loop={false}
+                        pagingEnabled={true}
+                        snapEnabled={true}
+                        mode='parallax'
+                        modeConfig={{
+                            parallaxScrollingScale: 0.9,
+                            parallaxScrollingOffset: 50,
+                        }}
+                        onProgressChange={progress}
+                        renderItem={renderItem}
+                    />
+                    <Pagination.Basic
+                        progress={progress}
+                        data={entry.pictures}
+                        size={8}
+                        dotStyle={styles.paginationDot}
+                        activeDotStyle={styles.activePaginationDot}
+                        containerStyle={styles.paginationRow}
+                        onPress={onPressPagination}
+                    />
                 </View>
-                {/* Measurement and Notes */}
-                {entry.measurement && <Text style={styles.headerText}>Measurement: {measurement}</Text>}
-                <View>
-                    <Text style={styles.headerText}>Notes:</Text>
-                    <Text style={styles.bodyText}>{entry.notes}</Text>
+                }
+                <View style={styles.textContainer}>
+                    {/* General Information */}
+                    <View style={styles.generalInfo}>
+                        <Text style={styles.titleText}>{entry.title}</Text>
+                        <Text style={styles.bodyText}>for {pet_names}</Text>
+                        <Text style={styles.bodyText}>with {caretaker_names}</Text>
+                        <Text style={styles.bodyText}>on {date} at {time}</Text>
+                    </View>
+                    {/* Measurement and Notes */}
+                    {entry.measurement && <Text style={styles.headerText}>Measurement: {measurement}</Text>}
+                    <View>
+                        <Text style={styles.headerText}>Notes:</Text>
+                        <Text style={styles.bodyText}>{entry.notes}</Text>
+                    </View>
                 </View>
             </ScrollView>
-        </View>
+        </GestureHandlerRootView>
     )
 }
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-    },
-    container: {
-        flex: 1,
         backgroundColor: THEME.COLOR_WHITE,
+    },
+    carouselContainer: {
+        backgroundColor: THEME.COLOR_WHITE,
+    },
+    paginationRow: {
+        gap: 4,
+    },
+    paginationDot: {
+        borderRadius: 100,
+        backgroundColor: THEME.COLOR_LIGHT_GREY,
+    },
+    activePaginationDot: {
+        backgroundColor: THEME.COLOR_MEDIUM_GREY,
+    },
+    textContainer: {
         margin: 16,
+        marginTop: 12,
     },
     generalInfo: {
         marginBottom: 16,
