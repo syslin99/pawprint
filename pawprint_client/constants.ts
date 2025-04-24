260b2f9321845fd250560ffb4845aa2cdd194467
Samantha Lin <linsam@engr.orst.edu>
2025-04-24 14:12:34 -0700

    Add KINDS as constants

--- pawprint_client/constants.ts
+++ pawprint_client/constants.ts
@@ -1,4 +1,5 @@
 import { THEME } from '@/theme';
+import { Kind } from '@/api_interfaces';
 
 
 export const FAKE_ID = Number.NEGATIVE_INFINITY;
@@ -7,6 +8,26 @@ export const FAKE_ID = Number.NEGATIVE_INFINITY;
 export const MONTHS_ABBR = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 export const MONTHS_FULL = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 export const DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+export const KINDS : {[key:number] : Kind}  = {
+    1 : {id: 1, name: 'Other', category: 'Other'},
+    2 : {id: 2, name: 'Meal', category: 'Food and Water'},
+    3 : {id: 3, name: 'Water', category: 'Food and Water'},
+    4 : {id: 4, name: 'Treat', category: 'Food and Water'},
+    5 : {id: 5, name: 'Groom', category: 'Care'},
+    6 : {id: 6, name: 'Brush Teeth', category: 'Care'},
+    7 : {id: 7, name: 'Clip Nails', category: 'Care'},
+    8 : {id: 8, name: 'Walk', category: 'Activity'},
+    9 : {id: 9, name: 'Train', category: 'Activity'},
+    10 : {id: 10, name: 'Play', category: 'Activity'},
+    11 : {id: 11, name: 'Pee', category: 'Activity'},
+    12 : {id: 12, name: 'Poop', category: 'Activity'},
+    13 : {id: 13, name: 'Weight', category: 'Health'},
+    14 : {id: 14, name: 'Temperature', category: 'Health'},
+    15 : {id: 15, name: 'Heart Rate', category: 'Health'},
+    16 : {id: 16, name: 'Respiratory Rate', category: 'Health'},
+    17 : {id: 17, name: 'Medicine', category: 'Health'},
+    18 : {id: 18, name: 'Vet Appointment', category: 'Health'},
+};
 export const KIND_ICONS : {[key:string] : string}  = {
     'Meal': 'utensils',
     'Water': 'tint',
